afd22a63af16c23f890a3cbcc797845d1236b646
Foster McLane <fkmclane@gmail.com>
2019-09-21 06:54:24 -0400

    add initial incomplete and non-working macOS support
    Note: I've abandoned this but I decided to push it to the repo anyway.
    If I'm remembering correctly, this did create a USB similar to running
    it on other *nixes, but it did not boot due to fundamental problems in
    the way macOS was partitioning it.

--- mkusb.sh
+++ mkusb.sh
@@ -1,11 +1,16 @@
 #!/bin/sh -e
-if [ "$(id -u)" -ne 0 ]; then
+if [ "`id -u`" -ne 0 ]; then
 	echo "error: script must be run as root"
 	exit 1
 fi
 
 # whether to partition with fat or ext2
 fat=1
+
+if [ $fat -eq 0 ] && [ "`uname`" == "Darwin" ]; then
+	echo "error: only fat is supported on macOS"
+	exit 1
+fi
 
 # get distro configuration
 distros="$1"
@@ -17,27 +22,37 @@ fi
 dev="$2"
 if [ -z "$dev" ]; then
 	echo "getting device..."
-	devs="$(find /dev/disk/by-path | grep -- '-usb-' | grep -v -- '-part[0-9]*$' || true)"
+	if [ "`uname`" == "Darwin" ]; then
+		devs="`for dev in \`diskutil list | grep '^/' | cut -d' ' -f1\`; do diskutil info $dev | grep 'Removable$' >/dev/null && echo $dev; done`"
+	else
+		devs="`find /dev/disk/by-path | grep -- '-usb-' | grep -v -- '-part[0-9]*$' || true`"
+	fi
 
 	if [ -z "$devs" ]; then
 		echo "error: no usb device found"
 		exit 2
 	fi
 
-	devs="$(readlink -f $devs)"
+	if [ "`uname`" == "Linux" ]; then
+		devs="`readlink -f $devs`"
+	fi
 
 	dialogdevs=""
 
 	dialogmodel=""
 	for dialogdev in $devs; do
-		dialogmodel="$(lsblk -ndo model "$dialogdev")"
+		if [ "`uname`" == "Darwin" ]; then
+			dialogmodel="`diskutil info $dialogdev | grep 'Device / Media Name:' | sed -e 's#   Device / Media Name:      ##'`"
+		else
+			dialogmodel="`lsblk -ndo model "$dialogdev"`"
+		fi
 		dialogdevs="$dialogdevs $dialogdev '$dialogmodel' off"
 	done
 	unset dialogdev
 	unset dialogmodel
 
 	while [ -z "$dev" ]; do
-		dev="$(eval "dialog --stdout --radiolist 'select usb device' 12 40 5 $dialogdevs")"
+		dev="`eval "dialog --stdout --radiolist 'select usb device' 12 40 5 $dialogdevs"`"
 		if [ "$?" -ne "0" ]; then
 			exit
 		fi
@@ -61,19 +76,42 @@ if [ -z "$efipart" ] || [ -z "$livepart" ]; then
 
 	# partition device
 	echo "partitioning..."
+	if [ "`uname`" == "Darwin" ]; then
+		diskutil unmountDisk "$dev" &>/dev/null
+	fi
 	dd if=/dev/zero of="$dev" count=$secstart &>/dev/null
-	sfdisk "$dev" >/dev/null <<EOF
+	if [ "`uname`" == "Darwin" ]; then
+		total=`diskutil info "$dev" | grep "Disk Size" | cut -d\( -f2 | cut -d' ' -f1`
+		size=`expr $total - $secstart '*' 512 - $secefi '*' 512`
+
+		diskutil partitionDisk "$dev" 2 MBR FAT32 %noformat% $size EFI %noformat% R >/dev/null
+
+		unset size
+		unset total
+
+		livepart="$dev"s1
+		efipart="$dev"s2
+	else
+		total=`blockdev --getsz "$dev"`
+		size=`expr $total - $secstart - $secefi`
+
+		sfdisk "$dev" >/dev/null <<EOF
 label: dos
 device: $dev
 unit: sectors
 
-${dev}1 : start=$secstart, size=$(expr $(blockdev --getsz "$dev") - $secstart - $secefi), type=b
+${dev}1 : start=$secstart, size=$size, type=b
 ${dev}2 : size=$secefi, type=ef
 EOF
-	blockdev --rereadpt "$dev"
 
-	livepart="$dev"1
-	efipart="$dev"2
+		unset size
+		unset total
+
+		blockdev --rereadpt "$dev"
+
+		livepart="$dev"1
+		efipart="$dev"2
+	fi
 
 	unset secstart
 	unset secefi
@@ -92,18 +130,29 @@ fi
 
 # mount devie
 echo "mounting..."
-livemnt="$(mktemp -d)"
-efimnt="$(mktemp -d)"
-mount "$livepart" "$livemnt"
-mount "$efipart" "$efimnt"
+livemnt="`mktemp -d`"
+efimnt="`mktemp -d`"
+if [ "`uname`" == "Darwin" ]; then
+	mount -t msdos "$livepart" "$livemnt"
+	mount -t msdos "$efipart" "$efimnt"
+else
+	mount "$livepart" "$livemnt"
+	mount "$efipart" "$efimnt"
+fi
 
-trap "(set +e; umount '$livemnt'; rmdir '$livemnt'; umount '$efimnt'; rmdir '$efimnt') &>/dev/null" EXIT
+if [ "`uname`" == "Darwin" ]; then
+	trap "(set +e; diskutil unmount '$livemnt'; rmdir '$livemnt'; diskutil unmount '$efimnt'; rmdir '$efimnt') &>/dev/null" EXIT
+else
+	trap "(set +e; umount '$livemnt'; rmdir '$livemnt'; umount '$efimnt'; rmdir '$efimnt') &>/dev/null" EXIT
+fi
 
 unset efipart
 
 # install grub
 echo "installing grub..."
-grub-install --target=i386-pc --boot-directory="$efimnt" "$dev" >/dev/null
+if [ "`uname`" == "Linux" ]; then
+	grub-install --target=i386-pc --boot-directory="$efimnt" "$dev" >/dev/null
+fi
 grub-install --target=x86_64-efi --boot-directory="$efimnt" --efi-directory="$efimnt" --removable >/dev/null
 
 unset dev
@@ -118,13 +167,21 @@ label() {
 	label="$1"
 
 	if [ -n "$formatted" ]; then
-		umount "$livepart"
+		if [ "`uname`" == "Darwin" ]; then
+			diskutil unmount "$livepart"
+		else
+			umount "$livepart"
+		fi
 		if [ $fat -ne 0 ]; then
 			fatlabel "$livepart" "$label" >/dev/null
 		else
 			e2label "$livepart" "$label" >/dev/null
 		fi
-		mount "$livepart" "$livemnt"
+		if [ "`uname`" == "Darwin" ]; then
+			mount -t msdos "$livepart" "$livemnt"
+		else
+			mount "$livepart" "$livemnt"
+		fi
 	fi
 }
 
@@ -132,19 +189,19 @@ iso() {
 	echo -e "\t$1"
 	cat >>"$livemnt"/grub.cfg <<EOF
 menuentry '$1' {
-	set filename=/$(basename "$2")
+	set filename=/`basename "$2"`
 	set label=$label
 	loopback iso \$filename
 EOF
 
 	IFS='#' read kernel64 kernel32 <<EOF
-$(echo "$3" | sed -e 's/[[:space:]]*--[[:space:]]*/#/g')
+`echo "$3" | sed -e 's/[[:space:]]*--[[:space:]]*/#/g'`
 EOF
 	IFS='#' read initrd64 initrd32 <<EOF
-$(echo "$4" | sed -e 's/[[:space:]]*--[[:space:]]*/#/g')
+`echo "$4" | sed -e 's/[[:space:]]*--[[:space:]]*/#/g'`
 EOF
 	IFS='#' read param64 param32 <<EOF
-$(echo "$5" | sed -e 's/%\([^%]\+\)%/$\1/g' | sed -e 's/[[:space:]]*--[[:space:]]*/#/g')
+`echo "$5" | sed -e 's/%\([^%]\+\)%/$\1/g' | sed -e 's/[[:space:]]*--[[:space:]]*/#/g'`
 EOF
 
 	if [ -n "$kernel32" ]; then
@@ -159,16 +216,16 @@ EOF
 		cat >>"$livemnt"/grub.cfg <<EOF
 	if cpuid -l; then
 		linux (iso)$kernel64 $param64
-		initrd$(for initrd in $initrd64; do echo -n " (iso)$initrd"; done)
+		initrd`for initrd in $initrd64; do echo -n " (iso)$initrd"; done`
 	else
 		linux (iso)$kernel32 $param32
-		initrd$(for initrd in $initrd32; do echo -n " (iso)$initrd"; done)
+		initrd`for initrd in $initrd32; do echo -n " (iso)$initrd"; done`
 	fi
 EOF
 	else
 		cat >>"$livemnt"/grub.cfg <<EOF
 	linux (iso)$kernel64 $param64
-	initrd$(for initrd in $initrd64; do echo -n " (iso)$initrd"; done)
+	initrd`for initrd in $initrd64; do echo -n " (iso)$initrd"; done`
 EOF
 	fi
 
@@ -184,7 +241,7 @@ EOF
 
 EOF
 
-	iso="$livemnt/$(basename "$2")"
+	iso="$livemnt/`basename "$2"`"
 
 	([ ! -e "$iso" ] || [ "$iso" -ot "$2" ]) && cp "$2" "$iso"
 
@@ -194,8 +251,12 @@ EOF
 refind() {
 	echo -e "\t$1"
 
-	refindmnt="$(mktemp -d)"
-	mount -o ro "$2" "$refindmnt"
+	refindmnt="`mktemp -d`"
+	if [ "`uname`" == "Darwin" ]; then
+		mount -t cd9660 -o ro "$2" "$refindmnt"
+	else
+		mount -o ro "$2" "$refindmnt"
+	fi
 
 	cat >>"$livemnt"/grub.cfg <<EOF
 if [ \${grub_platform} == "efi" ]; then
@@ -219,11 +280,19 @@ EOF
 	cp -r "$refindmnt" "$livemnt"/refind
 	rm -rf "$livemnt"/refind/EFI
 
-	umount "$refindmnt"
+	if [ "`uname`" == "Darwin" ]; then
+		diskutil unmount "$refindmnt"
+	else
+		umount "$refindmnt"
+	fi
 	unset refindmnt
 }
 
-source "$(readlink -f $distros)"
+if [ "`uname`" == "Darwin" ]; then
+	source "$distros"
+else
+	source "`readlink -f "$distros"`"
+fi
 
 # configure grub
 echo "configuring grub..."
@@ -262,8 +331,13 @@ sync
 
 # unmount
 echo "unmounting..."
-umount "$efimnt"
-umount "$livemnt"
+if [ "`uname`" == "Darwin" ]; then
+	diskutil unmount "$efimnt"
+	diskutil unmount "$livemnt"
+else
+	umount "$efimnt"
+	umount "$livemnt"
+fi
 rmdir "$efimnt"
 rmdir "$livemnt"
 
